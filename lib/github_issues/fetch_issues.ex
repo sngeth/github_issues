66b873abcaab9b476136e3c780a943c3e2316d70
Sid Ngeth <sngeth@gmail.com>
2017-01-20 10:19:29 -0500

    Add logging

--- lib/github_issues/fetch_issues.ex
+++ lib/github_issues/fetch_issues.ex
@@ -1,8 +1,11 @@
 defmodule GithubIssues.FetchIssues do
+  require Logger
+
   @user_agent [ {"User-agent", "Elixir dave@pragprog.com"} ]
   @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
+    Logger.info "Fetching user #{user}'s project #{project}"
     issues_url(user, project)
     |> HTTPoison.get!(@user_agent)
     |> handle_response
@@ -12,6 +15,14 @@ defmodule GithubIssues.FetchIssues do
     "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
-  def handle_response(%{status_code: 200, body: body}), do: {:ok, :jsx.decode(body)}
-  def handle_response(%{status_code: ___, body: body}), do: {:error, :jsx.decode(body)}
+  def handle_response(%{status_code: 200, body: body}) do
+    Logger.info "Success response"
+    Logger.debug fn -> inspect(body) end
+    {:ok, :jsx.decode(body)}
+  end
+
+  def handle_response(%{status_code: status, body: body}) do
+    Logger.error "Error #{status} returned"
+    {:error, :jsx.decode(body)}
+  end
 end
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule GithubIssues.Mixfile do
     [app: :github_issues,
      version: "0.1.0",
      elixir: "~> 1.3",
-     escript: escript_config,
+     escript: escript_config(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -15,7 +15,7 @@ defmodule GithubIssues.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :httpoison]]
+    [applications: [:logger, :httpoison, :jsx]]
   end
 
   # Dependencies can be Hex packages:
